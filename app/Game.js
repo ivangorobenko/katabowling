5c84f44d2fa972aeac344738fa35211d337d1f98
Fouad <tounes5959@gmail.com>
2021-03-11 15:43:41 +0100

    updated

--- app/Game.js
+++ app/Game.js
@@ -1,12 +1,32 @@
+
+const calculateScoreForAPlayer = require('../app/bowlingScoreCalculator.js');
 class Game {
+
     constructor(rollProvider) {
         this.rollProvider = rollProvider;
+        this.launchArray = [];
     }
     //rollProvider.getRoll() => nombre de quilles qui sont tombées
     // play => rollProvider.getRoll()=> gameTours[[]]
     play() {
+        const nbOfFallenPins = this.rollProvider.getRoll();
+        console.log("Nb of fallen pins : ",nbOfFallenPins)
+        const lastPLayedFrame = this.launchArray[this.launchArray.length-1];
+    
+        console.log("Last played frame : ", lastPLayedFrame);
+        if(lastPLayedFrame!==undefined && lastPLayedFrame.length==1){
+            lastPLayedFrame.push(nbOfFallenPins);
+        }
+        else {
+            this.launchArray.push([nbOfFallenPins]);
+        }
+        console.log("Launch array : ", this.launchArray);
     }
 
-    getScore() {
+    getScore(){
+        return calculateScoreForAPlayer(this.launchArray);
     }
+
 }
+
+module.exports = Game;
--- tests/Game.spec.js
+++ tests/Game.spec.js
@@ -1,10 +1,42 @@
 const expect = require('chai').expect;
-const Game = require('../app/Game.js');
+const Game = require('../app/Game');
 
-describe('Bowling', () => {
+let game; 
+
+describe('Game', () => {
+    let rolls;
+    let nbOfRolls;
+
+    beforeEach(() => {    
+        nbOfRolls = 0;
+
+        const rollProviderStub = {
+            getRoll : () => {
+                const roll = rolls[nbOfRolls];
+                nbOfRolls++;
+                return roll;
+            }
+        }
+
+        game = new Game(rollProviderStub);
+    });
+
     context('Expect', () => {
-        it('doit ', () => {
-            expect("").to.equal(1)
+        it('doit ajouter le nombre de quilles tombées 4 à la première lancée quand un joueur joue pour la première fois', () => {
+            rolls = [4];
+            
+            game.play()
+            expect(game.getScore()).to.equal(4)
         });
+        it('doit ajouter le nombre de quilles tombées 5 à la première lancée et 1 à la deuxième lancée quand un joueur joue pour la première fois', () => {
+            rolls = [5, 1];
+            
+            game.play()
+            expect(game.getScore()).to.equal(5)
+
+            game.play()
+            expect(game.getScore()).to.equal(6)
+        });
+        
     })
 })
